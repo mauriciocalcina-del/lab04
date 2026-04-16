1839f931b23d64857d1f0c6776c2f6398bc3b6f9
Mauricio Calcina <mauricio.calcina@tecsup.edu.pe>
2026-04-16 09:17:44 -0500

    feat: add text and button component

--- MovieCounter/app/src/main/java/com/example/moviecounter/MainActivity.kt
+++ MovieCounter/app/src/main/java/com/example/moviecounter/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.example.moviecounter
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.moviecounter.ui.theme.MovieCounterTheme
-Text(text = "Hola Curso")
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.ui.unit.dp
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,10 +25,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
+    Column(modifier = modifier.padding(16.dp)) {
+
+        Text(text = "Hola Curso")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { }) {
+            Text("Click me")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "Hello $name!")
+    }
 }
 
 @Preview(showBackground = true)

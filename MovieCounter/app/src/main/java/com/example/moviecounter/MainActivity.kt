c627e420b5673c73d94dd4f6e67b056d2d54d2b5
Mauricio Calcina <mauricio.calcina@tecsup.edu.pe>
2026-04-16 10:19:00 -0500

    feat: add second component

--- MovieCounter/app/src/main/java/com/example/moviecounter/MainActivity.kt
+++ MovieCounter/app/src/main/java/com/example/moviecounter/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.moviecounter
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.moviecounter.ui.theme.MovieCounterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +35,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
 
         Text(text = "Hola Curso")
-
+        Text(text = "Segundo componente")
+        
         Spacer(modifier = Modifier.height(16.dp))
 
         Button(onClick = { }) {
